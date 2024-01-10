a1682d81352d3e9ba547307d28309f36e4353a41
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-10 17:14:32 +0300

    props tried

--- sbilisim/src/App.js
+++ sbilisim/src/App.js
@@ -9,23 +9,32 @@ import Body from './0-Components/Body';
 
 function App() {
      
-  const [value, setValue] = useState(0);
-
+  // const [value, setValue] = useState(0);
   // function Decrase() {setValue(value-1)}
-  const Decrase=()=> setValue(value-1)
-  
+  // const Decrase=()=> setValue(value-1)
+  const [header, setHeader]=useState("I am learning React");
+  const yoB = 1970;  
     
     return (
        <div className='App'> 
          {/* <Navbar1/>
          <AltNavbar/>
          <Body/> */}
-         <p><button onClick={()=>{setValue(value+1)}}>Increase</button></p>
-         <p>{value}</p>
+         {/* <p><button onClick={()=>{setValue(value+1)}}>Increase</button></p>
+         <p>{value}</p> */}
          {/* <p><button onClick={()=>{setValue(value-1)}}>Decrease</button></p> */}
-         <p><button onClick={Decrase}>Decrease</button></p>
-
+         {/* <p><button onClick={Decrase}>Decrease</button></p> */}
+         <Container title={header} number={yoB}/>
        </div>
+      );
+    }
+
+    function Container(props){
+      return(
+        <div style={{width:"400px",height:"400px",border:"2px solid black", margin:"0px auto"}}>
+          <p>{props.title}</p> - 
+          <p>{props.number}</p>
+        </div>
       );
     }
 
