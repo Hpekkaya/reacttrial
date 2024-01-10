8bb68ae803028a746ba601d491701603af466826
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-10 16:53:17 +0300

    Functional Component applied

--- sbilisim/src/App.js
+++ sbilisim/src/App.js
@@ -10,6 +10,10 @@ import Body from './0-Components/Body';
 function App() {
      
   const [value, setValue] = useState(0);
+
+  // function Decrase() {setValue(value-1)}
+  const Decrase=()=> setValue(value-1)
+  
     
     return (
        <div className='App'> 
@@ -18,10 +22,14 @@ function App() {
          <Body/> */}
          <p><button onClick={()=>{setValue(value+1)}}>Increase</button></p>
          <p>{value}</p>
-         <p><button onClick={()=>{setValue(value-1)}}>Decrease</button></p>
+         {/* <p><button onClick={()=>{setValue(value-1)}}>Decrease</button></p> */}
+         <p><button onClick={Decrase}>Decrease</button></p>
+
        </div>
       );
     }
+
+
     
     export default App;
 
