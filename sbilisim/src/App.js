e1d470eddeee1ccc86a19bf272327e6440aa93fb
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-10 11:54:05 +0300

    sbilism created

---
+++ sbilisim/src/App.js
@@ -0,0 +1,19 @@
+
+import JSX from "./1-JSX/JSX"
+
+// import './index.css';
+
+
+function App() {
+    return (
+       <div>
+          <p>This is the First React Application </p> 
+          <JSX/> 
+          
+       </div>
+      );
+    }
+    
+    export default App;
+
+
